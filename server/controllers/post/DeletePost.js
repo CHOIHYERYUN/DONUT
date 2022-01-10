ccda5e4ac7815b312ec47b091cd9f613d0dfb5e9
HyeRyun Choi <violetmouce@gmail.com>
2022-01-10 22:27:10 +0900

    fix: DeletePost

--- server/controllers/post/DeletePost.js
+++ server/controllers/post/DeletePost.js
@@ -4,13 +4,15 @@ const { isAuthorized } = require('../tokenfunction');
 
 module.exports = (req, res) => {
   const authorization = isAuthorized(req);
-  //const {postId: post_id} = req.params; // ! 클라이언트와 통신하면서 체크 필요
 
   if(!authorization) {
     res.status(401).send({message: 'Invalid token!'});
   } else {
     // delete 메소드 사용(client)
     // destroy(sequelize method)
-    //await post.destroy({where: post_id}); // ! 클라이언트와 통신하면서 체크 필요
+    //await post.destroy({where: post_id});
+    post.destroy({where: {id: req.body.id}});
+
+    res.status(201).json({message: 'Delete Success!'});
   }
 };
